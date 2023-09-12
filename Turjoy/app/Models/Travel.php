a34ada3774dd4a5cef860c5a16bfa2a548892389
nagorik <fernando.valenzuela01@alumnos.ucn.cl>
2023-09-12 09:41:00 -0300

    (3) Modelo,Controladory Tablas creadas

---
+++ Turjoy/app/Models/Travel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Travel extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'origin',
+        'destiny',
+        'date',
+        'time',
+        'price',
+        'seat'
+    ];
+}
--- Turjoy/database/migrations/2023_09_12_121905_create_travel_table.php
+++ Turjoy/database/migrations/2023_09_12_121905_create_travel_table.php
@@ -11,15 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('travel', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->string('rol');
+            $table->string('origin');
+            $table->string('destiny');
             $table->timestamps();
+            $table->integer('seat');
+
         });
     }
 
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('travel');
     }
 };
