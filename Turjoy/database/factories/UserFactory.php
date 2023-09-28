6998400ceb14aac35c95bbb6a727ed5c076c3583
nagorik <fernando.valenzuela01@alumnos.ucn.cl>
2023-09-27 23:43:19 -0300

    modificados:     database/factories/UserFactory.php

--- Turjoy/database/factories/UserFactory.php
+++ Turjoy/database/factories/UserFactory.php
@@ -20,9 +20,9 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'role' => fake()->randomElement(['admin', 'user']),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+
         ];
     }
 
