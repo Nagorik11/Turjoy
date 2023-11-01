c8b3516190f4b6255bcf0a1beb5008591e09e1f7
nagorik <nagorik@machine.machine>
2023-10-31 22:12:10 -0300

    new file:   resources/views/searchVoucher.blade.php 	new file:   resources/views/voucherInformation.blade.php

---
+++ Turjoy/resources/views/searchVoucher.blade.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buscar Reserva</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="{!! url('assets/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet">
+</head>
+<body>
+    <div class="card m-5 mt-4">
+        <div class="card-header" style="background-color: lightblue;">
+            <h1 class="">Buscar Reservas</h1>
+            
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-4  d-flex align-items-center offset-md-2 justify-content-sm-start">
+                    <p class="fs-4 m-0">Ingrese el codigo de la reserva:</p>
+                </div>
+                <div class="col-md-4 d-flex align-items-center ">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Codigo de reserva" aria-label="Codigo de reserva" aria-describedby="button-addon2">
+                        <button class="btn btn-outline-primary" type="button" id="button-addon2">Buscar</button>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <div class="row card mx-2">
+                <table class="table p-5 m-0">
+                    <tbody>
+                        <tr>
+                            crear vista de la tabla si el resultado del voucher es correcto
+                            crear errores para cuando el voucher no exista
+                        </tr>
+                        <tr>
+
+</body>
+</html>
+
