2a3a13352dccd05e5912aeacf4ad65ea57f1c77c
nagorik <nagorik@machine.machine>
2023-11-02 11:55:01 -0300

    modified:   resources/views/reservation.blade.php

---
+++ Turjoy/resources/views/reservation.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap core CSS -->
+    <link href="{!! url('assets/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet">
+</head>
+<body>
+    <div class="card m-5 mt-4">
+        <div class="card-header" style="background-color: lightblue;">
+            <h1>Reservar Pasaje</h1>
+        </div>
+        <div class="card-body">
+            <form action="#" method="POST">
+            @csrf
+   
+        </select>
+    </div>
+    <button type="submit" class="btn btn-primary">Confirmar</button>
+</form>
+        </div>
+    </div>
+</body>
+</html>
