c2aad510bcf81b675e632d7ed76b9cbabd8c292b
MatiasFontecillaUCN <matias.fontecilla@alumnos.ucn.cl>
2023-11-11 18:48:40 -0300

    fix: "buscar" button position on searchVoucher view

--- Turjoy/resources/views/searchVoucher.blade.php
+++ Turjoy/resources/views/searchVoucher.blade.php
@@ -4,9 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{!! url('assets/bootstrap/css/bootstrap.min.css') !!}">
-    <link rel="stylesheet" href="//cdn.tutorialjinni.com/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="//cdn.tutorialjinni.com/bootstrap-select/1.12.4/css/bootstrap-select.min.css">
-
     <style>
         .card {
             height: 800px;
