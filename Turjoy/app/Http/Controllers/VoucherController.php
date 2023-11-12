a7383fd1b0a526c0d9f03e9ea959a87b5793ecf5
AleUCN <alexander.estay@alumnos.ucn.cl>
2023-11-11 21:01:18 -0300

    Resolved the file conlfict in the merge

--- Turjoy/app/Http/Controllers/VoucherController.php
+++ Turjoy/app/Http/Controllers/VoucherController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Voucher;
-use App\Models\Route;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Factory as ValidationFactory;
+use App\Helpers\MyHelper;
 
 
 class VoucherController extends Controller
@@ -19,56 +19,78 @@ class VoucherController extends Controller
 
     public function searchVoucher(Request $request)
     {
-
-
+        //dd($request->input('search_code'));
+        $message = errorMessages();
+        $request->validate([
+            'search_code' => 'required', //
+        ],$message);
         $code = $request->input('search_code');
-
+        //dd($code);
         //Busqueda en la BD del voucher
         $voucher = Voucher::where('id', $code)->first();
 
         if ($voucher) {
-            $route = Route::find($voucher->Route_id);
-
-            if ($route) {
-
-                return view('searchVoucher', ['voucher' => $voucher, 'route' => $route]);
-            }
+            return view('searchVoucher', ['voucher' => $voucher, 'cost' => ($voucher->base_rate*$voucher->seat_quantity)]);
         }
         else{
             $request->validate([
-                'search_code' => 'required', // Max 5MB
+                'search_code' => [
+                    'required',
+                    function ($attribute, $value, $fail) {
+                        $fail("la reserva ". $value ." no existe en sistema");
+                    },
+                ],
             ]);
-            return redirect()->route("voucher.index",)->with('error');
+            return redirect()->route("voucher.index")->with('error');
         }
     }
+    
 
     public function voucherInformation()
     {
         return view('voucherInformation');
     }
 
+    public function postView()
+    {
+        return view('postView');
+    }
+
     public function store(Request $request)
     {
         $voucher = new Voucher();
         $voucher->id = $this->codeVoucherGen();
-        $voucher->travel_id = $request->travel_id;
+        $voucher->date = $request->input('date');
         $voucher->origin = $request->origin;
-        $voucher->destination = $request->destination;
-        $voucher->seats = $request->seats;
-        $voucher->total_cost = $request->total_cost;
+        $voucher->destiny = $request->destiny;
+        $voucher->seat_quantity = $request->input('seat_quantity');
+        $voucher->base_rate = $request->base_rate;
         $voucher->save();
         return redirect()->route('voucher.index');
+        
     }
 
     public function codeVoucherGen()
     {
-        $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = strlen($characters);
+        $letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $numbers = '0123456789';
+    
+        $letterLength = strlen($letters);
+        $numberLength = strlen($numbers);
+    
         $code = '';
-        for ($i = 0; $i < 10; $i++) {
-            $code .= $characters[rand(0, $charactersLength - 1)];
+    
+        // Generate 4 letters
+        for ($i = 0; $i < 4; $i++) {
+            $code .= $letters[rand(0, $letterLength - 1)];
         }
+    
+        // Generate 2 numbers
+        for ($i = 0; $i < 2; $i++) {
+            $code .= $numbers[rand(0, $numberLength - 1)];
+        }
+    
         return $code;
     }
 
-}
+};
