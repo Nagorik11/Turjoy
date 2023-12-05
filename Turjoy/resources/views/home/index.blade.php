80eae39cb2b41dbd16da988bbcaa488b375a0a2b
MatiasFontecillaUCN <matias.fontecilla@alumnos.ucn.cl>
2023-12-05 18:29:24 -0300

    style: load excel view

--- Turjoy/resources/views/home/index.blade.php
+++ Turjoy/resources/views/home/index.blade.php
@@ -1,77 +1,75 @@
 @extends('layouts.app-master')
 
 @section('content')
+    <style>
+        #custom-button {
+            border-width: 1px;
+        }
 
-<style>
-    #custom-button {
-        border-width: 1px;
-    }
+        #file-input {
+            display: none;
+        }
 
-    #file-input {
-        display: none;
-    }
+        #file-input-container {
+            margin-bottom: 10px;
+        }
 
-    #file-input-container {
-        margin-bottom: 10px;
-    }
-
-    .card {
-        margin: 20px;
-        padding: 20px;
-        border: 1px solid #ddd;
-        border-radius: 10px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    }
-</style>
+        .card {
+            margin: 20px;
+            padding: 20px;
+            border: 1px solid #ddd;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
+    </style>
 
     @auth
-    <div class="card">
-    <h1>Cargar rutas</h1>
-    <hr>
+        <div class="card">
+            <h1>Cargar rutas</h1>
+            <hr>
 
-    @error('archivo')
-        <div class="alert alert-danger mt-3">
-            <p class="m-0">{{ $message }}</p>
-        </div>
-    @enderror
 
-    <form action="{{ route('travel.check') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div id="file-input-container">
-            <input type="file" name="archivo" id="file-input" accept=".xlsx" hidden="hidden">
-            <button type="button" id="custom-button">Escoge un archivo</button>
-            <span id="custom-text">No has seleccionado ningún archivo</span>
+            <form action="{{ route('travel.check') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div id="file-input-container">
+                    <input type="file" name="archivo" id="file-input" accept=".xlsx" hidden="hidden">
+                    <button type="button" id="custom-button">Escoge un archivo</button>
+                    <span id="custom-text">No has seleccionado ningún archivo</span>
+                </div>
+                <button type="submit" class="btn btn-primary">Cargar archivo</button>
+            </form>
+            @error('archivo')
+                <div class="alert alert-danger mt-3">
+                    <p class="m-0">{{ $message }}</p>
+                </div>
+            @enderror
         </div>
-        <button type="submit" class="btn btn-primary">Cargar archivo</button>
-    </form>
-</div>
     @endauth
 
 
-<script>
-    const fileInputBtn = document.getElementById("file-input");
-    const customTxt = document.getElementById("custom-text");
-    const customBtn = document.getElementById("custom-button");
-    customBtn.addEventListener("click", function () {
-        fileInputBtn.click();
-    });
+    <script>
+        const fileInputBtn = document.getElementById("file-input");
+        const customTxt = document.getElementById("custom-text");
+        const customBtn = document.getElementById("custom-button");
+        customBtn.addEventListener("click", function() {
+            fileInputBtn.click();
+        });
 
-    fileInputBtn.addEventListener("change", function () {
-        if (fileInputBtn.files.length > 0) {
-            const fileName = fileInputBtn.files[0].name;
-            customTxt.innerHTML = fileName;
-        } else {
-            customTxt.innerHTML = "No has seleccionado ningún archivo";
-        }
-    });
-</script>
+        fileInputBtn.addEventListener("change", function() {
+            if (fileInputBtn.files.length > 0) {
+                const fileName = fileInputBtn.files[0].name;
+                customTxt.innerHTML = fileName;
+            } else {
+                customTxt.innerHTML = "No has seleccionado ningún archivo";
+            }
+        });
+    </script>
 
-@guest
+    @guest
 
-<div class=" mt-3 alert" style="background-color: #ff8a80; color:black;">
-    <p>No tienes acceso a este apartado</p>
-</div>
+        <div class=" mt-3 alert" style="background-color: #ff8a80; color:black;">
+            <p>No tienes acceso a este apartado</p>
+        </div>
 
-@endguest
-
+    @endguest
 @endsection
--- Turjoy/resources/views/importExportView.blade.php
+++ Turjoy/resources/views/importExportView.blade.php
@@ -1,66 +1,80 @@
 @extends('layouts.app-master')
 
 @section('content')
+    <style>
+        #custom-button {
+            border-width: 1px;
+        }
 
-    <div class="bg-light p-5 rounded">
-        @auth
+        #file-input {
+            display: none;
+        }
 
-        <p class="lead">Cargar rutas de viaje.</p>
+        #file-input-container {
+            margin-bottom: 10px;
+        }
+
+        .card {
+            margin: 20px;
+            padding: 20px;
+            border: 1px solid #ddd;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
+    </style>
+
+    @auth
+        <div class="card">
+            <h1>Cargar rutas</h1>
+            <hr>
 
 
-        {{-- @if(session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
-        @endif
+            <form action="{{ route('travel.check') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div id="file-input-container">
+                    <input type="file" name="archivo" id="file-input" accept=".xlsx" hidden="hidden">
+                    <button type="button" id="custom-button">Escoge un archivo</button>
+                    <span id="custom-text">No has seleccionado ningún archivo</span>
+                </div>
+                <button type="submit" class="btn btn-primary">Cargar archivo</button>
+            </form>
+            @error('archivo')
+                <div class="alert alert-danger mt-3">
+                    <p class="m-0">{{ $message }}</p>
+                </div>
+            @enderror
+            @error('archivo')
+                <div class="alert alert-danger mt-3">
+                    <p class="m-0">{{ $message }}</p>
+                </div>
+            @enderror
 
-        @if(session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif --}}
+            @if ($allRows)
+                <h2>Datos Cargados</h2>
 
-        <form action="{{ route('travel.check') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="form-group">
-                <label for="archivo">Selecciona un archivo XLSX:</label>
-                <input type="file" name="archivo" id="archivo" accept=".xlsx">
-            </div>
-            <button type="submit" class="btn btn-primary">Cargar archivo</button>
-        </form>
-
-        @error('archivo')
-            <div class="alert alert-danger mt-3">
-                <p class="m-0">{{ $message }}</p>
-            </div>
-        @enderror
-
-        @if($allRows)
-
-            <h2>Datos Cargados</h2>
-
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Origen</th>
-                        <th>Destino</th>
-                        <th>Cantidad de Asientos</th>
-                        <th>Tarifa Base</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($allRows as $row)
-                    <tr style="background-color: {{ $row['type'] == 0 ? '#a8e6cf' : ($row['type'] == 2 ? '#ff8a80' : ($row['type'] == 1 ? '#e4e6a8' : '')) }}">
-                            <td>{{ $row['origen'] }}</td>
-                            <td>{{ $row['destino'] }}</td>
-                            <td>{{ $row['cantidad_de_asientos'] }}</td>
-                            <td>{{ $row['tarifa_base'] }}</td>
+                <table class="table border border-1">
+                    <thead>
+                        <tr>
+                            <th>Origen</th>
+                            <th>Destino</th>
+                            <th>Cantidad de Asientos</th>
+                            <th>Tarifa Base</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        @endif
+                    </thead>
+                    <tbody>
+                        @foreach ($allRows as $row)
+                            <tr
+                                style="background-color: {{ $row['type'] == 0 ? '#a8e6cf' : ($row['type'] == 2 ? '#ff8a80' : ($row['type'] == 1 ? '#e4e6a8' : '')) }}">
+                                <td>{{ $row['origen'] }}</td>
+                                <td>{{ $row['destino'] }}</td>
+                                <td>{{ $row['cantidad_de_asientos'] }}</td>
+                                <td>{{ $row['tarifa_base'] }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @endif
         @endauth
     </div>
+    </div>
 @endsection
-
